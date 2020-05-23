bfdc49ceb848f9bf79ec87a4724ebf197efdd37d
Tyler Wilcock <tyler.l.wilcock@gmail.com>
2020-05-23 13:09:17 -0500

    Set default window size upon initialization

--- src/gfx/mod.rs
+++ src/gfx/mod.rs
@@ -1,5 +1,7 @@
+use crate::resize_window;
 use gl::util::opengl_version;
 use gl::Gl;
+use glutin::dpi::PhysicalSize;
 use glutin::event_loop::EventLoop;
 use glutin::window::WindowBuilder;
 use glutin::{ContextBuilder, GlProfile, PossiblyCurrent, WindowedContext};
@@ -10,17 +12,22 @@ pub mod paint;
 
 pub fn init_main_window_and_gl() -> (WindowedContext<PossiblyCurrent>, EventLoop<()>, Gl) {
     let el = EventLoop::new();
+    // This was an arbitrary choice in size.  We can revisit this later.
+    let initial_physical_size = PhysicalSize {
+        width: 1920,
+        height: 1080,
+    };
     let wb = WindowBuilder::new()
         .with_title("Kosmonaut")
-        .with_maximized(true);
+        .with_inner_size(initial_physical_size);
     let windowed_context = ContextBuilder::new()
         .with_gl_profile(GlProfile::Core)
         .build_windowed(wb, &el)
         .unwrap();
     let windowed_context = unsafe { windowed_context.make_current().unwrap() };
-    // TODO: Do we need to set gl.Viewport() size here?
     let gl_context = windowed_context.context();
     let gl = Gl::load_with(|ptr| gl_context.get_proc_address(ptr) as *const _);
+    resize_window(&gl, &windowed_context, &initial_physical_size);
     (windowed_context, el, gl)
 }
 

b697f37c543925af6e7a592d3057d057556ad50d
Tyler Wilcock <tyler.l.wilcock@gmail.com>
2021-01-17 10:35:31 -0600

    DRY out directional layout test path creation

--- tests/layout/directional/mod.rs
+++ tests/layout/directional/mod.rs
@@ -1,11 +1,16 @@
 use crate::layout::{dump_layout_cmd, DumpLayoutVerbosity};
 use insta::assert_snapshot;
 
+const DIRECTIONAL_WEBSRC_DIR: &str = "tests/websrc/directional";
+fn path(filename: &str) -> String {
+    format!("{}/{}", DIRECTIONAL_WEBSRC_DIR, filename)
+}
+
 #[test]
 fn ltr_vertical_lr_block_boxes() {
     let mut dump_layout_cmd = dump_layout_cmd(DumpLayoutVerbosity::NonVerbose);
     dump_layout_cmd
-        .arg("tests/websrc/directional/ltr-vertical-lr-block-boxes.html")
+        .arg(path("ltr-vertical-lr-block-boxes.html"))
         .succeeds();
     assert_snapshot!(dump_layout_cmd.stdout());
 }
@@ -14,7 +19,7 @@ fn ltr_vertical_lr_block_boxes() {
 fn ltr_vertical_lr_block_boxes_bottom_right_mbp_applied_physically() {
     let mut dump_layout_cmd = dump_layout_cmd(DumpLayoutVerbosity::Verbose);
     dump_layout_cmd
-        .arg("tests/websrc/directional/ltr-vertical-lr-block-boxes-bottom-right-mbp-applied-physically.html")
+        .arg(path("ltr-vertical-lr-block-boxes-bottom-right-mbp-applied-physically.html"))
         .succeeds();
     assert_snapshot!(dump_layout_cmd.stdout());
 }
@@ -23,7 +28,7 @@ fn ltr_vertical_lr_block_boxes_bottom_right_mbp_applied_physically() {
 fn ltr_vertical_lr_block_boxes_top_left_right_mbp_applied_physically() {
     let mut dump_layout_cmd = dump_layout_cmd(DumpLayoutVerbosity::Verbose);
     dump_layout_cmd
-        .arg("tests/websrc/directional/ltr-vertical-lr-block-boxes-top-left-mbp-applied-physically.html")
+        .arg(path("ltr-vertical-lr-block-boxes-top-left-mbp-applied-physically.html"))
         .succeeds();
     assert_snapshot!(dump_layout_cmd.stdout());
 }
@@ -32,7 +37,7 @@ fn ltr_vertical_lr_block_boxes_top_left_right_mbp_applied_physically() {
 fn rtl_horizontal_tb_block_boxes() {
     let mut dump_layout_cmd = dump_layout_cmd(DumpLayoutVerbosity::NonVerbose);
     dump_layout_cmd
-        .arg("tests/websrc/directional/rtl-horizontal-tb-block-boxes.html")
+        .arg(path("rtl-horizontal-tb-block-boxes.html"))
         .succeeds();
     assert_snapshot!(dump_layout_cmd.stdout());
 }
